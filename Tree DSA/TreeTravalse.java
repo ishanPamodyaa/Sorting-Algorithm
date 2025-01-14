4b2599dbad02b05a7a8c46640fcc256ace82f041
Ishan Pamodya <inishan9999@gmail.com>
2025-01-14 09:48:41 +0530

    preOrder Tree traversal

--- Tree DSA/TreeTravalse.java
+++ Tree DSA/TreeTravalse.java
@@ -11,19 +11,29 @@ public class TreeTravalse {
         tree.root.right = new Node(3);
         tree.root.left.left = new Node(4);
         tree.root.left.right = new Node(5);
-        tree.printPostOrder(tree.root);
-    
-    }
+        // tree.printPostOrder(tree.root);
+        tree.printPreOrder(tree.root);
+        // tree.printPostOrder(tree.root);
 
+    }
+//post order
     public void printPostOrder(Node node){
         if(node == null){
             return;
         }
-       
-
         printPostOrder(node.left);
         printPostOrder(node.right);
         System.out.print(" "+node.key);
+    }
+
+    //pre order 
+    public void printPreOrder(Node node){
+        if(node == null){
+            return;
+        }
+        System.out.print(" "+node.key);
+        printPreOrder(node.left);
+        printPreOrder(node.right);
     }
     
 }
